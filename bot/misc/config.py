9613cc93b84ecfd146738e8a47cc55779f57c964
chris4a4 <chris4a4@gmail.com>
2024-07-05 19:57:09 -0700

    added leaderboard to config

--- bot/misc/config.py
+++ bot/misc/config.py
@@ -6,6 +6,12 @@ class Config:
     BASE_URL = ''
     UNKNOWN_THUMBNAIL = ''
     LEADERBOARD_TOP_N = 0
+    TTR_ICON = ''
+    TTR_COLOR = 0
+    TTCC_ICON = ''
+    TTCC_COLOR = 0
+    OVERALL_ICON = ''
+    OVERALL_COLOR = 0
     WELCOME_CHANNEL = 0
     GUILD = 0
     AUTHORIZED_ROLE = ''
@@ -20,17 +26,29 @@ class Config:
 
     # Can be populated immediately
     @classmethod
-    def load_from_file(cls, filepath):
+    def load_config(cls, filepath):
         with open(filepath, 'r') as file:
             data = yaml.safe_load(file)
 
             cls.BASE_URL = data['BASE_URL']
             cls.UNKNOWN_THUMBNAIL = data['UNKNOWN_THUMBNAIL']
             cls.LEADERBOARD_TOP_N = data['LEADERBOARD_TOP_N']
+            cls.TTR_ICON = data['TTR_ICON']
+            cls.TTR_COLOR = data['TTR_COLOR']
+            cls.TTCC_ICON = data['TTCC_ICON']
+            cls.TTCC_COLOR = data['TTCC_COLOR']
+            cls.OVERALL_ICON = data['OVERALL_ICON']
+            cls.OVERALL_COLOR = data['OVERALL_COLOR']
             cls.WELCOME_CHANNEL = data['WELCOME_CHANNEL']
             cls.GUILD = data['GUILD']
             cls.AUTHORIZED_ROLE = data['AUTHORIZED_ROLE']
-            cls.TOKEN = data['TOKEN']
+    
+    def load_token(cls, filepath):
+        with open(filepath, 'r') as file:
+            cls.TOKEN = file.read()
 
 config_path = os.path.join(os.path.dirname(__file__), '..',  'config.yaml')
-Config.load_from_file(config_path)
+Config.load_config(config_path)
+
+token_path = os.path.join(os.path.dirname(__file__), '..',  'token.txt')
+Config.load_token(token_path)
