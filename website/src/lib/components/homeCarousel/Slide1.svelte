ef368282c783ef32603b02d27814268145d891e9
chris4a4 <chris4a4@gmail.com>
2024-07-20 14:35:24 -0700

    all changes

--- website/src/lib/components/homeCarousel/Slide1.svelte
+++ website/src/lib/components/homeCarousel/Slide1.svelte
@@ -3,7 +3,11 @@
   <div class="h-1 w-full m-2 bg-raisinblack"></div>
   <p class="text-justify text-sm sm:text-2xl mb-8">This website contains a collection of all the records, which includes the fastest times and highest scores for bosses, facilities, activities, and more! You will also find leaderboards that show us the Top Toons with the most points. Check them all out!</p>
   <div class="w-full flex justify-between">
-    <button class="btn-animate bg-raisinblack text-lightpink px-4 py-2 text-sm sm:text-2xl rounded-3xl">VIEW RECORDS</button>
-    <button class="btn-animate bg-raisinblack text-lightpink px-4 py-2 text-sm sm:text-2xl rounded-3xl">VIEW LEADERBOARDS</button>
+    <button class="btn-animate bg-raisinblack text-lightpink px-4 py-2 text-sm sm:text-2xl rounded-3xl">
+      <a href='/records'>VIEW RECORDS</a>
+    </button>
+    <button class="btn-animate bg-raisinblack text-lightpink px-4 py-2 text-sm sm:text-2xl rounded-3xl">
+      <a href='/leaderboards'>VIEW LEADERBOARDS</a>
+    </button>
   </div>
 </div>
--- website/src/lib/components/homeCarousel/Slide4.svelte
+++ website/src/lib/components/homeCarousel/Slide4.svelte
@@ -3,7 +3,11 @@
   <div class="h-1 w-full m-2 bg-white"></div>
   <p class="text-justify text-sm sm:text-2xl mb-8">On top of having a functioning website, we also keep track of submissions in our Discord server. If you’re interested in submitting records through the Discord Server, feel free to drop by there and verify yourself. You can also connect your Discord to the Website by logging in through our website with Discord authentication.</p>
   <div class="w-full flex justify-between">
-    <button class="btn-animate bg-black text-white px-4 py-2 text-sm sm:text-2xl rounded-3xl">VIEW RECORDS</button>
-    <button class="btn-animate bg-black text-white px-4 py-2 text-sm sm:text-2xl rounded-3xl">VIEW LEADERBOARDS</button>
+    <button class="btn-animate bg-black text-white px-4 py-2 text-sm sm:text-2xl rounded-3xl">
+      <a href='/#'>JOIN THE DISCORD</a>
+    </button>
+    <button class="btn-animate bg-black text-white px-4 py-2 text-sm sm:text-2xl rounded-3xl">
+      <a href='/#'>LOGIN TO WEBSITE</a>
+    </button>
   </div>
 </div>
