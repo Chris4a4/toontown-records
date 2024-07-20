c60ea0623b8ce5acaff6fa0a90f25c11b4e2a9ab
chris4a4 <chris4a4@gmail.com>
2024-07-20 13:10:01 -0700

    buttons on edge now

--- website/src/lib/components/homeCarousel/Slide1.svelte
+++ website/src/lib/components/homeCarousel/Slide1.svelte
@@ -2,7 +2,7 @@
   <h2 class="text-5xl text-center">Records and Leaderboards</h2>
   <div class="h-1 w-full m-2 bg-raisinblack"></div>
   <p class="text-2xl mb-8">This website contains a collection of all the records, which includes the fastest times and highest scores for bosses, facilities, activities, and more! You will also find leaderboards that show us the Top Toons with the most points. Check them all out!</p>
-  <div class="w-full flex justify-evenly">
+  <div class="w-full flex justify-between">
     <button class="btn-animate bg-raisinblack text-lightpink px-4 py-2 md:text-2xl rounded-3xl">VIEW RECORDS</button>
     <button class="btn-animate bg-raisinblack text-lightpink px-4 py-2 md:text-2xl rounded-3xl">VIEW LEADERBOARDS</button>
   </div>
--- website/src/lib/components/homeCarousel/Slide4.svelte
+++ website/src/lib/components/homeCarousel/Slide4.svelte
@@ -1,8 +1,8 @@
 <div class="flex flex-col items-center max-w-3xl font-righteous text-black">
   <h2 class="text-5xl text-center">Join Our Discord Server!</h2>
   <div class="h-1 w-full m-2 bg-white"></div>
-  <p class="text-2xl mb-8">On top of having a functioning website, we also keep track of submissions in our Discord server. If you’re interested in submitting records through the DIscord Server, feel free to drop by there and verify yourself. You can also connect your Discord to the Website by logging in through our website with Discord authentication.</p>
-  <div class="w-full flex justify-evenly">
+  <p class="text-2xl mb-8">On top of having a functioning website, we also keep track of submissions in our Discord server. If you’re interested in submitting records through the Discord Server, feel free to drop by there and verify yourself. You can also connect your Discord to the Website by logging in through our website with Discord authentication.</p>
+  <div class="w-full flex justify-between">
     <button class="btn-animate bg-black text-white px-4 py-2 md:text-2xl rounded-3xl">VIEW RECORDS</button>
     <button class="btn-animate bg-black text-white px-4 py-2 md:text-2xl rounded-3xl">VIEW LEADERBOARDS</button>
   </div>
