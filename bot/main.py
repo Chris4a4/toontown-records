fa255fcdd94a265b6c8ec79076e787f38ec20997
chris4a4 <chris4a4@gmail.com>
2024-08-06 09:05:26 -0700

    fix bot loading bad config if connection is bad

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,4 @@
 import discord
-from discord.ext import commands
 
 from singletons.config import Config
 from singletons.channel_managers import ChannelManagers
@@ -8,14 +7,19 @@ from misc.rate_limited_bot import RateLimitedBot
 
 bot = RateLimitedBot(intents=discord.Intents.all())
 
+first_load = True
+
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} ({bot.user.id})')
-    print('------')
+    if first_load:
+        print(f'Logged in as {bot.user.name} ({bot.user.id})')
+        print('------')
 
-    Config.add_context(bot)
-    ChannelManagers.initialize(bot)
-    bot.load_extension('cogs.force_update')  # This cog has to be loaded AFTER the bot knows which guilds its in
+        Config.add_context(bot)
+        ChannelManagers.initialize(bot)
+        bot.load_extension('cogs.force_update')  # This cog has to be loaded AFTER the bot knows which guilds its in
+
+        first_load = False
 
 
 bot.load_extension('cogs.commands')  # These cogs have to be loaded BEFORE bot.run in order to be registered with discord correctly
