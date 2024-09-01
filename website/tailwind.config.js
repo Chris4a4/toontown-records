c68122c37dd7638ecba0af65ac8bbce6f566fd43
chris4a4 <chris4a4@gmail.com>
2024-09-01 14:37:39 -0700

    new website nav

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,9 +1,34 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        minnie: ["Minnie"],
+        righteous: ["Righteous"],
+        poppins: ["Poppins"],
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      raisinblack: "#332E3C",
+      lightpink: "#D5C5C8",
+      royalblue: "#5A70BF",
+      emeraldgreen: "#77ACA2",
+      lavender: "#EAE8FF",
+      platinum: "#D8D5D8",
+      frenchgray: "#ADACB5",
+      gunmetal: "#2D3142",
+      uranianblue: "#B0D7FF",
+
+      white: colors.white,
+      blue: colors.blue,
+      gray: colors.gray,
+      black: colors.black,
+    },
   },
   plugins: [],
-}
-
+};
