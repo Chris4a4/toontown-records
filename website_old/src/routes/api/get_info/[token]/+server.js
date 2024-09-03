33e222baf6152224a9116b162aff74d015d310b5
chris4a4 <chris4a4@gmail.com>
2024-09-03 11:43:19 -0700

    remove old website

--- website_old/src/routes/api/get_info/[token]/+server.js
+++
@@ -1,5 +0,0 @@
-export function GET({ params }) {
-    const { token } = params;
-
-    return fetch(`${import.meta.env.VITE_BACKEND_URL}/api/oauth2/get_info/${token}`);
-}
--- website_old/src/routes/news/+page.svelte
+++
@@ -1,7 +0,0 @@
-<div class="flex w-full h-screen">
-  <div class="flex flex-col w-3/4 pr-4">
-    <div class="h-16 bg-gray-300 mb-4">afdssfda</div>
-    <div class="flex-grow bg-gray-400">hgffgh</div>
-  </div>
-  <div class="w-1/4 bg-gray-500">iuyyiu</div>
-</div>
